8f9f9999bc57ae6016ae1ca8db2724915a5cd884
4721126 <130112378+4721126@users.noreply.github.com>
2023-04-14 10:31:59 +0900

    Update Prog21.java

--- src/main/java/Prog21.java
+++ src/main/java/Prog21.java
@@ -1,11 +1,17 @@
+/**
+ * クラス Prog21 の注釈をここに書きます.
+ * 
+ * @author (あなたの名前)
+ * @version (バージョン番号もしくは日付)
+ */
 public class Prog21 {
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
         String sikiA = "x+y = ";
         String sikiB = "y/x = ";
-        System.out.println("x=" + 2 + ",y=" + 7 +"とすると、");
-        System.out.println(sikiA + x+y);
-        System.out.println(sikiB + y/x);
+        System.out.println("x=" + x + ",y=" + y +"とすると、");
+        System.out.println(sikiA + (x+y));
+        System.out.println(sikiB + (double)y/x);
     }
 }
